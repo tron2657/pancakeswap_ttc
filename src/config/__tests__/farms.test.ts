dde320d1401445bd7bfa6e63433efbc4bd6e2f05
TTSwap <MY_NAME@TTSwap.com>
2022-07-24 11:23:38 +0000

    1

--- src/config/__tests__/farms.test.ts
+++ src/config/__tests__/farms.test.ts
@@ -37,7 +37,7 @@ describe('Config farms', () => {
 
   // The first pid using the new factory
   const START_PID = 2
-  const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
+  const FACTORY_ADDRESS = '0x7F354e892855249090E4f60b115B000225675d20'
   const newFarmsToTest = farmsToTest.filter((farmSet) => farmSet[0] >= START_PID)
 
   it.each(newFarmsToTest)('farm %d is using correct factory address', async (pid, farm) => {
--- src/config/constants/exchange.ts
+++ src/config/constants/exchange.ts
@@ -4,7 +4,7 @@ import { mainnetTokens, testnetTokens } from './tokens'
 import { ChainTokenList } from './types'
 
 export const ROUTER_ADDRESS = {
-  [ChainId.MAINNET]: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+  [ChainId.MAINNET]: '0xd14AF17cc3152b3D7522FD1b2471b10764ceAd0f',
   [ChainId.TESTNET]: '0xD99D1c33F9fC3444f8101754aBC46c52416550D1',
 }
 
