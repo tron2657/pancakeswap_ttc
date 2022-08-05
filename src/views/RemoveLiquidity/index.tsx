b36a44b6bb5688d7d1f91fda2427740698c1e1cc
Your Name <you@example.com>
2022-08-05 22:13:01 +0800

    1

--- src/views/RemoveLiquidity/index.tsx
+++ src/views/RemoveLiquidity/index.tsx
@@ -146,7 +146,7 @@ export default function RemoveLiquidity() {
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'TTC LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
