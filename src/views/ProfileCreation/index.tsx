c4de60640e58d25dd1d880ef65c5e6a352cb90e0
刘俊勋 <liujunxun2003@gmail.com>
2022-08-13 00:26:12 +0800

    8-12

--- src/views/ProfileCreation/index.tsx
+++ src/views/ProfileCreation/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import Page from 'components/Layout/Page'
 import { useProfile } from 'state/profile/hooks'
@@ -7,17 +7,39 @@ import { nftsBaseUrl } from 'views/Nft/market/constants'
 import { useRouter } from 'next/router'
 import Header from './Header'
 import ProfileCreationProvider from './contexts/ProfileCreationProvider'
+import { DIVI_API } from 'config/constants/endpoints'
+
 import Steps from './Steps'
 
+const isAddressApi = async (account: string) => {
+  const res = await fetch(`${DIVI_API}/user/is_address?address=${account}`, {
+    method: 'get',
+  })
+  if (res.ok) {
+    const json = await res.json()
+    return json
+  }
+  console.error('Failed to fetch NFT collections', res.statusText)
+  return null
+}
 const ProfileCreation = () => {
   const { account } = useWeb3React()
   const { isInitialized, isLoading, hasProfile } = useProfile()
   const router = useRouter()
-
+  const [isNode, setIsNode] = useState(false)
   useEffect(() => {
     if (account && hasProfile) {
       router.push(`${nftsBaseUrl}/profile/${account.toLowerCase()}`)
     }
+    async function init() {
+      if (account) {
+        const _data = await isAddressApi(account)
+        if (_data.status) {
+          setIsNode(_data.result)
+        }
+      }
+    }
+    init()
   }, [account, hasProfile, router])
 
   if (!isInitialized || isLoading) {
@@ -28,8 +50,8 @@ const ProfileCreation = () => {
     <>
       <ProfileCreationProvider>
         <Page>
-          <Header />
-          <Steps />
+          <Header isNode={isNode} />
+          {/* <Steps /> */}
         </Page>
       </ProfileCreationProvider>
     </>
